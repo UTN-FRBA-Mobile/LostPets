65119e8bf2c0f81f4bba9d8606dbb831551929fe
Nicolás Melnyk <nmelnyk@frba.utn.edu.ar>
2022-06-10 00:30:59 -0300

    Auth con google funcionando

--- android/app/src/main/java/com/utn/lostpets/fragments/LoginFragment.kt
+++ android/app/src/main/java/com/utn/lostpets/fragments/LoginFragment.kt
@@ -1,14 +1,23 @@
 package com.utn.lostpets.fragments
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.utn.lostpets.MainActivity
 import com.utn.lostpets.R
 import com.utn.lostpets.databinding.FragmentLoginBinding
 
@@ -16,6 +25,8 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val GOOGLE_SIGN_IN = 100
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,7 +71,52 @@ class LoginFragment : Fragment() {
                         }
                     }
         }
+
+        /* Acción de "Acceder con Google" */
+        binding.googleButton.setOnClickListener{
+            val googleConf : GoogleSignInOptions =
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+
+            /* Cliente autenticación Google */
+            val googleClient : GoogleSignInClient = GoogleSignIn.getClient(requireContext(), googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SIGN_IN) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if(account != null) {
+
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(account.email ?: "")
+                            } else {
+                                showAlert()
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+        }
+    }
+
 
     /* Mostramos alerta en caso de que haya fallado la autenticación */
     private fun showAlert() {
