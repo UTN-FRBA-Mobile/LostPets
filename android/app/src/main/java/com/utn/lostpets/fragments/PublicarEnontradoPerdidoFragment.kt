ce34fd08053491416e93afac68ba80acbe60962e
acerbonimartin <acerboni_martin@hotmail.com>
2022-07-02 04:33:10 -0300

    funciona cargar imagen de galeria

--- android/app/src/main/java/com/utn/lostpets/fragments/PublicarEnontradoPerdidoFragment.kt
+++ android/app/src/main/java/com/utn/lostpets/fragments/PublicarEnontradoPerdidoFragment.kt
@@ -1,15 +1,20 @@
 package com.utn.lostpets.fragments
 
+import android.Manifest
+import android.R.attr
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Base64.encodeToString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import com.utn.lostpets.databinding.FragmentPublicarEnontradoPerdidoBinding
 import com.utn.lostpets.dto.PublicationDTO
@@ -31,6 +36,8 @@ class PublicarEnontradoPerdidoFragment : Fragment() {
 
     private val apiUrl = "http://www.mengho.link/publications/publicacion/";
     private var publicationImage: Bitmap? = null
+
+    val Fragment.packageManager get() = activity?.packageManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,6 +67,11 @@ class PublicarEnontradoPerdidoFragment : Fragment() {
     private fun showHola(algo : String) {
         Toast.makeText(activity, algo, Toast.LENGTH_SHORT).show()
     }
+
+
+    val CAMERA_REQUEST_CODE = 42
+    val GALLERY_REQUEST_CODE = 13
+    val GALLERY_PERMISSION_CODE = 1001;
 
     private fun setup() {
         /* Se vuelve a la pantalla de "Login" en caso de cerrarse la sesión */
@@ -108,14 +120,33 @@ class PublicarEnontradoPerdidoFragment : Fragment() {
         binding.cargarImagenButton.setOnClickListener{
             dispatchTakePictureIntent()
         }
+
+        binding.cargarImagenGaleriaButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(requireActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, GALLERY_PERMISSION_CODE)
+                } else{
+                    chooseImageGallery();
+                }
+            }else{
+                chooseImageGallery();
+            }
+        }
     }
 
-    val REQUEST_CODE = 42
+
+    private fun chooseImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+
 
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (takePictureIntent.resolveActivity(requireActivity()!!.packageManager) != null){
-            startActivityForResult(takePictureIntent,REQUEST_CODE)
+            startActivityForResult(takePictureIntent,CAMERA_REQUEST_CODE)
         } else{
             //Toast.makeText(this,"Unable to open camera",Toast.LENGTH_SHORT)
         }
@@ -123,11 +154,19 @@ class PublicarEnontradoPerdidoFragment : Fragment() {
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == CAMERA_REQUEST_CODE  && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             publicationImage = imageBitmap
             binding.cargarImagenImageView.setImageBitmap(imageBitmap)
         }
+        if  (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK) {
+            val imageData = data?.data
+            val imageUri: Uri = data!!.getData()!!
+            val imageBitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(), imageUri)
+            publicationImage = imageBitmap
+            binding.cargarImagenImageView.setImageBitmap(imageBitmap)
+        }
+
     }
 
     fun bitMapToString(bitmap: Bitmap): String? {
@@ -136,4 +175,21 @@ class PublicarEnontradoPerdidoFragment : Fragment() {
         val b: ByteArray = baos.toByteArray()
         return Base64.getEncoder().encodeToString(b)
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            GALLERY_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    chooseImageGallery()
+                }else{
+                    Toast.makeText(requireActivity(),"Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 }
